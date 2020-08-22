6f27ec751948a7a2e4fee40684b095def15f5987
Chi-Hung Le <lchhung@gmail.com>
2020-08-22 23:05:26 +0100

    JWT Security updated

--- src/main/java/com/lchhung/vehicles/domain/Car.java
+++ src/main/java/com/lchhung/vehicles/domain/Car.java
@@ -17,11 +17,20 @@ public class Car {
 	private int year, price;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ownerId")
+	@JoinColumn(name = "owner")
 	private Owner owner;
 
 	public Car() {}
 
+	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.year = year;
+		this.price = price;
+	}
 	public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
@@ -30,8 +39,10 @@ public class Car {
 		this.registerNumber = registerNumber;
 		this.year = year;
 		this.price = price;
-		this.owner = owner;
+		this.owner=owner;
 	}
+
+
 
 	public Owner getOwner() {
 		return owner;
@@ -78,5 +89,11 @@ public class Car {
 		this.price = price;
 	}
 
+	public long getId() {
+		return id;
+	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
 }
--- src/test/java/com/lchhung/vehicles/VihecleDatabaseApplicationTests.java
+++ src/test/java/com/lchhung/vehicles/VihecleDatabaseApplicationTests.java
@@ -1,13 +1,23 @@
 package com.lchhung.vehicles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.lchhung.vehicles.web.CarController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class VihecleDatabaseApplicationTests {
 
+	@Autowired
+	private CarController carController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(carController).isNotNull();
 	}
 
 }
